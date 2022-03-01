2f7014995817c16f9e13ff73d2250514d0df45f2
My-Kairo <marciatsoho@gmail.com>
2022-03-01 07:29:21 +0200

    functionality

--- factory.js
+++ factory.js
@@ -1,5 +1,22 @@
 function FlagCountries(countries){
     var flagz = countries || [];
 
-    
+    function addTheCountries (name){
+        let names = name.charAt(0).toUppercase() + name.slice(1).lowerCase();
+        if(!flagz.include(names)){
+            flagz.push(names)
+        }else{
+            return
+        }
+    }
+
+    function getCountries (){
+        return flagz;
+    }
+
+
+    return {
+        addTheCountries,
+        getCountries
+    }
 }
--- index.js
+++ index.js
@@ -3,14 +3,49 @@ const countries = ["Argentina", "Brazil", "Chile", "Zambia", "Uganda", "Malawi",
 const flags = ["🇦🇷", "🇧🇷", "🇨🇱", "🇿🇲", "🇺🇬", "🇲🇼", "🇷🇼", "🇮🇪", "🇨🇭"];
 
 const countryFlag = document.querySelector(".flags");
+const addBtnElem = document.querySelector(".addBtn");
+const addTextElem = document.querySelector(".addText");
+const displayElem = document.querySelector(".display");
+const sortElem = document.querySelector(".sorting");
 
 let list = '<ul>' + countries.map(function(country, index){
     return '<li>' + country + '  ' + '-' + '  ' + flags[index] + '</li>';
 }).join('') + '</ul>';
 
-let addCountry = [];
-
-
-
-
 countryFlag.innerHTML = list;
+
+
+let adding = [];
+
+if(localStorage['name']){
+  let names = JSON.parse(localStorage.getItem('name'))
+}
+
+let countryInstance = FlagCountries(names);
+
+function countryAdded () {
+   const regex = /[a-zA-z]$/g;
+
+   if(!regex.test(addTextElem.value)){
+    displayElem.classList.add("red");
+     return displayElem.innerHTML = "Enter letters from A to Z!"
+   }
+   else {
+     countryInstance.addTheCountries(addTextElem.value)
+     displayElem.classList.remove("red")
+
+     let key = countryInstance.getCountries();
+     localStorage.setItem('name', JSON.stringify(key));
+   }
+   location.reload();
+   console.log(key)
+   
+}
+addBtnElem.addEventListener('click', countryAdded);
+
+
+function sorted (){
+  let sortCountry = countries.sort();
+  addTextElem.innerHTML = {addTextElem: sortCountry};
+}
+sortElem.removeEventListener('click', sorted);
